d9d954af7eed07e5117d052185a732215f2ae983
pinkxz <victor666157@gmail.com>
2025-06-22 20:37:35 -0300

    Exclusão Mutua: Lock

--- ExclusoesMutuas/Turn/main.cpp
+++ ExclusoesMutuas/Turn/main.cpp
@@ -1,31 +1,51 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 
-// Area critica 1
 
-void critica1(){
+int lock = 0;
+
+
+void critica1(std::vector<int>& v){
+    while(lock == 1);
+    lock = 1;
+    // Area critica 1
     for(int i = 0; i < 10000; i++){
-        std::cout << "Loop1: " << i << std::endl;
+        v.push_back(i);
     }
+    lock = 0;
 
 }
 
 
 
-
-// Area critica 2
-void critica2(){
-    for(int i = 0; i < 10000; i++){
-        std::cout << "Loop2: " << i << std::endl;
+void critica2(std::vector<int>& v){
+    while(lock == 0);
+    // Area critica 2
+    lock = 0;
+    for(int i = 0; i < 1000; i++){
+        v.push_back(i);
     }
+    lock = 1;
 }
 
 
 // Main
 int main(){
-    std::thread t1(critica1);
-    std::thread t2(critica2);
+    std::vector<int> vetorCompartilhado;
+
+
+    std::thread t1(critica1,  std::ref(vetorCompartilhado));
+    std::thread t2(critica2,  std::ref(vetorCompartilhado));
     t1.join();
     t2.join();
+
+    std::cout << "Tamanho final do vetor: " << vetorCompartilhado.size() << std::endl;
+    std::cout.flush();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cerr << "Terminou as threads\n";
+
+
     return 0;
 }
