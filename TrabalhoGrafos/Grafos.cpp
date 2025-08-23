482ca8720b7fbba613f77054c9c84674728a1eb1
pinkxz <victor666157@gmail.com>
2025-08-23 10:46:56 -0300

    Desenho dinâmico de grafos Floyd-Warshall

---
+++ TrabalhoGrafos/Grafos.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <random>
+#include <fstream>
+using namespace std;
+
+void generateGraphvizFile(const std::string& filename, const vector<vector<int>>& matrizC) {
+    std::ofstream file(filename);
+
+    file << "graph G {\n";
+    int vertices = matrizC.size();
+
+    for (int i = 0; i < vertices; i++) {
+        for (int j = i+1; j < vertices; j++) { // j > i evita duplicar
+            if (matrizC[i][j] < 100000) { // existe aresta
+                file << "  " << i << " -- " << j 
+                     << " [label=\"" << matrizC[i][j] << "\"];\n";
+            }
+        }
+    }
+
+    // desenhar os nós como círculos
+    for (int i = 0; i < vertices; i++) {
+        file << "  " << i << " [shape=circle];\n";
+    }
+
+    file << "}\n";
+    file.close();
+}
+
+int main() {
+    int vertices;
+
+
+    cout << "Digite o número de vertices: ";
+    cin >> vertices;
+
+
+    // Declarando uma matriz de inteiros com vetor de vetor
+    vector<vector<int>> matrizI(vertices, vector<int>(vertices));
+    vector<vector<int>> matrizC(vertices, vector<int>(vertices));
+
+    // Preenchendo a matriz
+    cout << "Digite os elementos da matriz:\n";
+    for (int i = 0; i < vertices; i++) {
+        for (int j = 0; j < vertices; j++) {
+            cout << "Elemento [" << i << "][" << j << "]: ";
+            cin >> matrizI[i][j];
+
+        }
+    }
+
+    // Exibindo a matriz
+    cout << "\nMatriz Incidencia:\n";
+    for (int i = 0; i < vertices; i++) {
+        for (int j = 0; j < vertices; j++) {
+            cout << matrizI[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < vertices; i++){
+        for (int j = 0; j < vertices; j++){
+            if(matrizI[i][j] == 0){
+                matrizC[i][j] = 100000;
+            }else{
+                matrizC[i][j] = rand() % 5 + 1;
+            }
+        }
+    }
+    cout << "\nMatriz Custo:\n";
+    for (int i = 0; i < vertices; i++) {
+        for (int j = 0; j < vertices; j++) {
+             cout << matrizC[i][j] << "\t";
+         }
+         cout << endl;
+    }
+
+
+    // Floyd-Warshall
+    for(int k = 0; k < vertices; k++){
+     //   cout << "Loop K" << k << endl;
+        for(int i = 0; i < vertices; i++){
+          //  cout << "Loop I" << i << endl;
+            for(int j = 0; j < vertices; j++){
+           //     cout << "Loop J" << j << endl;
+           if(i == j){
+            continue;
+           }
+                if(matrizC[i][k] + matrizC[k][j] < matrizC[i][j]){
+            //       cout << "comparando" << endl;
+                    matrizC[i][j] = matrizC[i][k] + matrizC[k][j];
+                }
+            }
+        }
+    }
+
+    cout << "\nMatriz CustoFinal:\n";
+    for (int i = 0; i < vertices; i++) {
+        for (int j = 0; j < vertices; j++) {
+            cout << matrizC[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    generateGraphvizFile("graph.dot", matrizC);
+    return 0;
+
+}
